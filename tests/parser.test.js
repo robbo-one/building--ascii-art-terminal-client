887da0c556941b33d47afab9dd13616cbad65cb3
Joshua Vial <joshua@enspiral.com>
2017-05-01 13:36:16 +1200

    better naming in test

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -4,14 +4,14 @@ var parser = require('../lib/parser')
 test('q closes parser', (t) => {
   var fakeRL = {
     close: () =>  {
-      t.pass()
+      t.pass('rl.close() called successfully')
       t.end()
     }
   }
   parser.parseInput('q', {rl: fakeRL})
 })
 
-test('prompts if input is invalid', (t) => {
+test('calls presenter.invalidInput() if input is invalid', (t) => {
   t.plan(2)
   var fakePresenter = {
     invalidInput: () => {
