09ea64ba9778a6d565574d11fea9e67607a4aeda
Joshua Vial <joshua@enspiral.com>
2017-05-01 13:45:52 +1200

    parser reads in correct art

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -21,3 +21,25 @@ test('calls presenter.invalidInput() if input is invalid', (t) => {
   parser.parseInput('0', {presenter: fakePresenter, files: ['a', 'b']})  
   parser.parseInput('7', {presenter: fakePresenter, files: ['a', 'b']})  
 })
+
+test('prints art if valid art is chosen', (t) => {
+  t.plan(3)
+  var fakePresenter = {
+    printArt: (txt, printer) => {
+      t.equal(txt, 'fake text from file', 'print art is passed the correct art')
+      t.equal(printer, 'fake printer', 'printer is passed in from params')
+    }
+  }
+  var fakeReader = {
+    readArt: (fileName, callback) => {
+      t.equal(fileName, 'a', 'reader is passed the correct file')
+      callback('fake text from file')
+    }
+  }
+  parser.parseInput('1', {
+    presenter: fakePresenter,
+    reader: fakeReader,
+    printer: 'fake printer',
+    files: ['a']
+  })
+})
