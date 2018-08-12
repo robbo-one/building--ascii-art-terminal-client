792b125bb60a31550ca32642c543a4369df92e55
Rich Churcher <rich.churcher@gmail.com>
2018-08-13 10:18:53 +1200

    2. Show a list

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const fs = require('fs')
+
 console.log('Welcome to ASCII Art Reader!')
+
+fs.readdir('data', listFiles)
+
+function listFiles (err, files) {
+  if (err) {
+    console.log('Sorry, could not read from the data directory.')
+    return
+  }
+  console.log('Here are the available files:')
+  files.forEach(showListItem)
+}
+
+function showListItem (item) {
+  console.log(` - ${item}`)
+}
