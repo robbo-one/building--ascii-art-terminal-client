162ff8ec553e96c52e1e3f5ca8894f43b429b7a7
Rich Churcher <rich.churcher@gmail.com>
2018-08-13 12:09:46 +1200

    Test debugging

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 console.log('Welcome to ASCII Art Reader!')
-
+console.log('Here are the available files:')
 fs.readdir('data', listFiles)
 
 function listFiles (err, files) {
@@ -9,10 +9,14 @@ function listFiles (err, files) {
     console.log('Sorry, could not read from the data directory.')
     return
   }
-  console.log('Here are the available files:')
   files.forEach(showListItem)
 }
 
 function showListItem (item) {
   console.log(` - ${item}`)
+}
+
+module.exports = {
+  listFiles,
+  showListItem
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,20 @@
+const fs = require('fs')
+const { showListItem } = require('../')
+
+beforeAll(() => {
+  // Spy on console.log to test its output:
+  global.console = { log: jest.fn() }
+})
+
 test('Tests are configured correctly.', () => expect(true).toBe(true))
+
+test('showListItem shows a list item', () => {
+  // What I expect to see
+  const expected = ' - wombat.txt'
+
+  // What actually happens
+  showListItem('wombat.txt')
+
+  // Evaluate: do they match?
+  expect(console.log).toHaveBeenCalledWith(expected)
+})
