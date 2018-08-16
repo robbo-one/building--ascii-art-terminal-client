efa918ef4d1abaa000bd1daeb42ccc8fe72e2639
Rich Churcher <rich.churcher@gmail.com>
2018-08-16 13:11:50 +1200

    Demonstrate one option for flow control

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-const fs = require('fs')
+// This is a (reasonably) complex solution to the ascii-art-reader challenge.
+// Many people struggle with "flow control" in console programs using
+// callbacks, or how to pass control from one function to another without
+// ending up in "callback hell". This demonstrates one approach, but it might
+// be confusing for those who aren't already comfortable with passing functions
+// around as values. Use your best judgement!
 
-console.log('Welcome to ASCII Art Reader!')
-console.log('Here are the available files:')
-fs.readdir('data', listFiles)
+const display = require('./display')
+const files = require('./files')
+const store = require('./store')
 
-function listFiles (err, files) {
-  if (err) {
-    console.log('Sorry, could not read from the data directory.')
-    return
+// This is an IIFE (Immediately Invoked Function Expression). It's basically a
+// function that "calls itself"! You can read more about them here:
+// https://medium.com/@vvkchandra/essential-javascript-mastering-immediately-invoked-function-expressions-67791338ddc6
+;(function asciiArtReader () {
+  let actionIndex = 0
+
+  // Notice that each 'action' is a function name?
+  // Read out loud, this also provides us with a nice description of what
+  // happens when a user runs our program.
+  const actions = [
+    files.loadDirectory,
+    display.choices,
+    display.input
+  ]
+
+  function next () {
+    // Exit if there's no more work to do
+    if (actionIndex === actions.length) {
+      return
+    }
+
+    // Get the next function to call from the list above
+    const action = actions[actionIndex]
+
+    // 'Point' to the next action in the list
+    actionIndex++
+
+    // Call the function, passing it `next` so it can call it when it's done,
+    // and `reset` in case it needs to start everything over.
+    action(next, reset)
   }
-  files.forEach(showListItem)
-}
 
-function showListItem (item) {
-  console.log(` - ${item}`)
-}
+  // Provide a way to start from scratch
+  function reset () {
+    actionIndex = 0
+    next()
+  }
 
-module.exports = {
-  listFiles,
-  showListItem
-}
+  // Get this party started
+  next()
+}())
