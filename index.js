9e28df21001886e348721172dd8ebf07f6d61c2a
student <student@eda-bc-09.localdomain>
2021-01-11 12:25:18 +1300

    we did it

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+
+
 const fs = require('fs')
 
 function welcome () {
-  console.log('Welcome!!!!!')
+  console.log('Welcome!')
 }
 welcome()
 
 
 
-// PSUEDOCODE
-// write function to return each individual artwork when prompted by number
-
-
-function chooseArt(){
+//************Print List of Art
+function listArt(){
 fs.readdir ('./data', 'utf8', (err,fileContents) => {
   if (err) {
     console.log("something went wrong",err)
@@ -19,7 +18,58 @@ fs.readdir ('./data', 'utf8', (err,fileContents) => {
     for(i = 0; i < fileContents.length; i++){
       console.log(i + " " + fileContents[i])
     }
+
   }
 })
 }
-chooseArt()
+
+//***********Accept user input
+function pressEnter () {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question('Which picture do you want to see? Pick a number.', function (input) {
+    rl.close()
+    // Call any functions you like here. For example:
+   printArt(input)
+  })
+}
+
+
+//************* Show Art
+function printArt(input){
+const artFile = pickANumber(input)
+fs.readFile(artFile, 'utf8', (err,fileContents) => {
+  if (err){
+    console.log('something went wrong', err)
+  } else {
+    console.log(fileContents)
+  }
+})}
+
+
+//********** Return selcted image
+function pickANumber (num) {
+  if (num == 0) {
+    return 'data/kea.txt'
+  } if (num == 1) {
+    return 'data/kiwi.txt'
+  } if (num == 2) {
+    return 'data/manaia.txt'
+  } if (num == 3) {
+    return 'data/nikau.txt'
+  } if (num == 4) {
+    return 'data/pohutukawa.txt'
+  } 
+}
+
+const readline = require('readline')
+
+
+pressEnter()
+listArt()
+//printArt()
+
+
