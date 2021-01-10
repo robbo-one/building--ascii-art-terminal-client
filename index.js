a6abdec14e75f6c456470ac40e3c260b20ce45a2
Lukas <63496113+lukas-jj@users.noreply.github.com>
2021-01-11 12:41:17 +1300

    finish stretch

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ fs.readdir('data', 'utf8', (err, fileContents) => {
     })
 })
 
+function menu() {
+    fs.readdir('data', 'utf8', (err, fileContents) => {
+
+        fileContents.map((val, i) => {
+            console.log(val, i)
+        })
+    })
+}
+
 // const readline = require('readline')
 const readline = require('readline')
 
@@ -30,6 +39,14 @@ function pressEnter() {
 
 }
 
+function repeat() {
+
+    menu()
+    pressEnter()
+
+}
+
+
 pressEnter()
 
 function getArt(input) {
@@ -37,29 +54,47 @@ function getArt(input) {
     if (input == 0) {
         fs.readFile('./data/kea.txt', 'utf8', (err, fileContents) => {
             console.log(fileContents)
+            repeat()
         })
     }
     else if (input == 1) {
         fs.readFile('./data/kiwi.txt', 'utf8', (err, fileContents) => {
             console.log(fileContents)
+            repeat()
         })
     }
     else if (input == 2) {
         fs.readFile('./data/manaia.txt', 'utf8', (err, fileContents) => {
             console.log(fileContents)
+            repeat()
         })
     }
     else if (input == 3) {
         fs.readFile('./data/nikau.txt', 'utf8', (err, fileContents) => {
             console.log(fileContents)
+            repeat()
         })
     }
     else if (input == 4) {
         fs.readFile('./data/pohutukawa.txt', 'utf8', (err, fileContents) => {
             console.log(fileContents)
+            repeat()
         })
     }
+    else if (input == "q") {
+        process.exit()
+    }
+    else if (input == "c") {
+        const data = " and I am happy"
+        fs.appendFile('data/comments.txt', data, 'utf8', (err) => {
+            if (err) throw err;
+            console.log(data);
+        });
+    }
+
     else {
-        console.log("No such number")
+        console.log("No artwork for that number. Try again.")
+        repeat()
     }
 }
+
