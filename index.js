16fdb81b021747dc6458df175b156e8acec93f53
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-01-11 12:53:58 +1300

    we done yo

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
 const fs = require('fs')
+const readline = require('readline')
 
 console.log("Hi! Welcome to our Art Space!")
 
-let artMenu = ["kea.txt: 1", "kiwi.txt: 2", "manaia.txt: 3", "nikau.txt: 4", "pohutakawa.txt: 5"]
+let artMenu = ["kea.txt", "kiwi.txt", "manaia.txt", "nikau.txt", "pohutukawa.txt"]
 
 console.log(artMenu)
 
-// let one = artMenu[0]
-// let two = artMenu[1]
-// let three = artMenu[2]
-// let four = artMenu[3]
-// let five = artMenu[4]
+let artValues = {
+1: artMenu[0],
+2: artMenu[1],
+3: artMenu[2],
+4: artMenu[3],
+5: artMenu[4],
+}
 
+function artLoop () {
+for (i = 0; i < artValues.length; i++) {
+  return[i]
+  }
+}
 
 // user enters desired number of Artwork, function loads that specific artwork
 
-// function loadArt 
-fs.readFile('./data/kea.txt', 'utf8', (err, fileContents) => {
+function loadArt (input) {
+fs.readFile('./data/' + artValues[input], 'utf8', (err, fileContents) => {
   if (err) {
-    console.log('oppsies', err)
+    console.log('oopsies', err)
   } else {
     console.log('here is the bird of your choice.')
     console.log('contents!', fileContents)
+  }
+})
+}
 
+/*
+fs.readdir('./data', 'utf8', (err, dirContents) => {
+  if (err) {
+    console.log('oppsies', err)
+  } else {
+  console.log('Choose from one of these:', dirContents)
   }
 
-})
+})*/
 
 
-
-
-
-
+//User input needs to happen here
 
 
 // const readline = require('readline')
 
-// function pressEnter () {
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-//   })
+function input () {
+const rl = readline.createInterface({
+     input: process.stdin,
+    output: process.stdout
+  })
 
-//   rl.question('Which file should I load? ', function (input) {
-//     rl.close()
-
-//     // Call any functions you like here. For example:
-//     loadFile(input)
-//   })
-// }
+  rl.question('Which artwork would you like?', function (input) {
+    rl.close()
+    console.log(input) 
+    //Call any functions you like here. For example:
+    loadArt(input)
+    
+  })
+  }
+input()
