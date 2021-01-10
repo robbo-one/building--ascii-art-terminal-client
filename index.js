e0b0e52862ef4b903869bd12f0afb3a56612a113
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-11 10:45:04 +1300

    Getting input from terminal.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const fs = require("fs")
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
 
 console.log("Hi, welcome to ASCII artworks! :) ")
 fs.readdir("./data", 'utf-8', (err, fileList) => {
   if(err) {
     console.log("this is an error! " + err)
   } else {
-    console.log(fileList)
+    for(let i = 0; i < fileList.length; i++){
+      console.log("\t" + i + ": " + fileList[i])
+    }
+    rl.question("What file do you want to view?", (answer) => {
+      console.log("This is you answer: " + answer)
+    })
   }
 })
+
+
+
+
 
