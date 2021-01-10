6b306933b6bae48af8fa8b45005b5f4af6a79f74
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-11 12:42:55 +1300

    stretch goal 4 attempted

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-const fs = require('fs')
-const readline = require('readline')
+const fs = require("fs");
+const readline = require("readline");
 
-console.log('Kia ora!')
+console.log("Kia ora!");
 
-let newArray = []
+showList();
 
-fs.readdir('./data', 'utf8', (err, fileContents) => {
-  
-  if (err) {
-    console.log('Something went wrong: ', err)
-  } else {
-    newArray = [...fileContents]
-    console.log(newArray)
-    for(let i = 0; i < fileContents.length; i++) {
-      console.log(i +": ", fileContents[i])
+let newArray = [];
+
+function showList() {
+  fs.readdir("./data", "utf8", (err, fileContents) => {
+    if (err) {
+      console.log("Something went wrong: ", err);
+    } else {
+      newArray = [...fileContents];
+      console.log(newArray);
+      for (let i = 0; i < fileContents.length; i++) {
+        console.log(i + ": ", fileContents[i]);
+      }
+      getInput();
     }
-    pressEnter()
-  }
-}) 
+  });
+}
 
-
-function pressEnter () {
+function getInput() {
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
-  })
+    output: process.stdout,
+  });
 
-
-  rl.question('Which file should I load? \n', function (input) {
-    rl.close()
+  rl.question("Which file should I load? \n", function (input) {
+    rl.close();
 
     // Call any functions you like here. For example:
-    loadFile(input)
-    //console.log(input)
-  })
-}
-
-function loadFile(input) { 
-  
-  fs.readFile("./data/"+newArray[input], "utf8", (err, fileContents) => {
-    if(err) {
-      console.log("Something went wrong: ", err)
-    }else {
-      console.log(fileContents)
+    //do handling here
+    
+    if (parseInt(input) >= 0 && parseInt(input) <= 4) {
+      loadFile(input);
     }
-  } )
+    
+  });
 }
 
-//console.log(newArray)
-// pressEnter()
-
-
-
-
-
-
-
-
-
+function loadFile(input) {
+    fs.readFile("./data/" + newArray[input], "utf8", (err, fileContents) => {
+      if (err) {
+        console.log("Something went wrong: ", err);
+      } else {
+        console.log(fileContents);
+        showList()
+      }
+    });
+  
+}
