d75acb8cea9fc92ee80877efab56c68f64610965
Lukas <63496113+lukas-jj@users.noreply.github.com>
2021-01-11 12:09:40 +1300

    finish and tidy

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function welcomeMessage(user) {
-
     console.log("Welcome " + user)
-
 }
 
 welcomeMessage("Henry")
@@ -12,15 +10,7 @@ fs.readdir('data', 'utf8', (err, fileContents) => {
     fileContents.map((val, i) => {
         console.log(val, i)
     })
-
 })
-
-fs.readFile('./data/kea.txt', 'utf8', (err, fileContents) => {
-
-    // console.log(fileContents)
-
-})
-
 
 // const readline = require('readline')
 const readline = require('readline')
@@ -34,55 +24,42 @@ function pressEnter() {
     rl.question('Which file should I load? ', function (input) {
         rl.close()
 
-        // Call any functions you like here. For example:
+        // Call any functions you like here.
         getArt(input)
     })
+
 }
 
-console.log(pressEnter(1))
-
+pressEnter()
 
 function getArt(input) {
 
     if (input == 0) {
         fs.readFile('./data/kea.txt', 'utf8', (err, fileContents) => {
-
             console.log(fileContents)
-
         })
     }
     else if (input == 1) {
         fs.readFile('./data/kiwi.txt', 'utf8', (err, fileContents) => {
-
             console.log(fileContents)
-
         })
     }
-
     else if (input == 2) {
         fs.readFile('./data/manaia.txt', 'utf8', (err, fileContents) => {
-
             console.log(fileContents)
-
         })
     }
     else if (input == 3) {
         fs.readFile('./data/nikau.txt', 'utf8', (err, fileContents) => {
-
             console.log(fileContents)
-
         })
     }
     else if (input == 4) {
         fs.readFile('./data/pohutukawa.txt', 'utf8', (err, fileContents) => {
-
             console.log(fileContents)
-
         })
     }
     else {
-        return null
+        console.log("No such number")
     }
-
-
 }
