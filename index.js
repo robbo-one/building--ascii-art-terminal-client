bbf6e697fc69e0e7c2df6cdd49b151663f18ad42
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-01-11 19:45:52 +1300

    Adjust order of functions and add comments

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
 const readline = require('readline')
 
-console.log("Hi! Welcome to our Art Space!")
-
 let artMenu = ["kea.txt", "kiwi.txt", "manaia.txt", "nikau.txt", "pohutukawa.txt"]
+
 let realMenu = {
   'kea 1': artMenu[0],
   'Kiwi 2': artMenu[1],
@@ -11,76 +10,72 @@ let realMenu = {
   'Nikau 4': artMenu[3],
   'Pohutukawa 5': artMenu[4]
 }
-console.log(realMenu)
 
 let artValues = {
-1: artMenu[0],
-2: artMenu[1],
-3: artMenu[2],
-4: artMenu[3],
-5: artMenu[4],
+  1: artMenu[0],
+  2: artMenu[1],
+  3: artMenu[2],
+  4: artMenu[3],
+  5: artMenu[4],
 }
 
-function mainMenu () {
-  console.log(realMenu)
+console.log("Hi! Welcome to our Art Space!") //1=Starting point
+
+console.log(realMenu) //2=Displays realMenu with numbers/txt file accessed from artMenu indice
+
+ //Call any functions you like here. For example:
+ input() //3=calls input function
+
  
-}
-
-// function artLoop () {
-// for (i = 0; i < artValues.length; i++) {
-//   return[i]
-//   }
-// }
-
-// user enters desired number of Artwork, function loads that specific artwork
-
-function loadArt (input) {
-fs.readFile('./data/' + artValues[input], 'utf8', (err, fileContents) => {
-  if (err) {
-    console.log('oopsies', err)
-  } else {
-    console.log('here is the art of your choice.')
-    console.log('contents!', fileContents)
-    menuInput()
-  }
-})
-}
-
-//User input needs to happen here
-
-
-// const readline = require('readline')
-
-function input () {
-const rl = readline.createInterface({
-     input: process.stdin,
+//FUNCTIONS
+function input () { //4=input asks for input.User asked for number
+  const rl = readline.createInterface({
+    input: process.stdin,
     output: process.stdout
   })
-  rl.question('Type number to load Artwork & press ENTER?', function (input) {
+  rl.question('Type number to load Artwork & press ENTER?', function (input) {//input = number
     rl.close()
     console.log(input)    
-    //Call any functions you like here. For example:
-    loadArt(input)    
+    loadArt(input)  //5= loadArt executed  
   })
 }
-   //Call any functions you like here. For example:
-input()
 
+function loadArt (input) {
+  fs.readFile('./data/' + artValues[input], 'utf8', (err, fileContents) => {
+    if (err) {
+      console.log('Oopsies!', err)
+    } else {
+      console.log('here is the art of your choice.')
+      console.log('contents!', fileContents)
+      menuInput() //6=menuInput called 
+    }
+  })
+}
 
-
-function menuInput () {
+function menuInput () {//7=user asked for 'menu' or 'q'
   const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-    rl.question('Type \'Menu\' to return to Menu\n or \'q\' to quit >', function (start) {
-      rl.close()
-      if (start === 'menu') { 
-     mainMenu()
-     input()
-      }
-      else if 
+  rl.question('Type \'Menu\' to return to Menu\n or \'q\' to quit >', function (choice) {//menu or q
+    rl.close()
+    if (choice === 'menu') { 
+      mainMenu() //8a=mainMenu called and realMenu displayed. User selects number.
+      input() //10=input called. loops back to Line 31.
+    } else if 
       (input === 'q') 
-       process.exit()
-      })
-    }
+      process.exit()//8b=exit from programm
+  })
+}
+
+function mainMenu () {//9=mainMenu executed - realMenu displayed and user selects number 
+  console.log(realMenu)
+}
+
+
+
+  
+
+
+
+ 
