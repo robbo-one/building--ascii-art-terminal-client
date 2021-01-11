4a39883cc2309a394b5b59896829d983ea135110
Lukas <63496113+lukas-jj@users.noreply.github.com>
2021-01-11 15:25:10 +1300

    refactor funcs

--- index.js
+++ index.js
@@ -12,14 +12,6 @@ fs.readdir('data', 'utf8', (err, fileContents) => {
     })
 })
 
-function menu() {
-    fs.readdir('data', 'utf8', (err, fileContents) => {
-
-        fileContents.map((val, i) => {
-            console.log(val, i)
-        })
-    })
-}
 
 // const readline = require('readline')
 const readline = require('readline')
@@ -32,69 +24,33 @@ function pressEnter() {
 
     rl.question('Which file should I load? ', function (input) {
         rl.close()
-
         // Call any functions you like here.
         getArt(input)
     })
-
 }
-
-function repeat() {
-
-    menu()
-    pressEnter()
-
-}
-
 
 pressEnter()
 
+
+
 function getArt(input) {
 
-    if (input == 0) {
-        fs.readFile('./data/kea.txt', 'utf8', (err, fileContents) => {
-            console.log(fileContents)
-            repeat()
-        })
-    }
-    else if (input == 1) {
-        fs.readFile('./data/kiwi.txt', 'utf8', (err, fileContents) => {
-            console.log(fileContents)
-            repeat()
-        })
-    }
-    else if (input == 2) {
-        fs.readFile('./data/manaia.txt', 'utf8', (err, fileContents) => {
-            console.log(fileContents)
-            repeat()
-        })
-    }
-    else if (input == 3) {
-        fs.readFile('./data/nikau.txt', 'utf8', (err, fileContents) => {
-            console.log(fileContents)
-            repeat()
-        })
-    }
-    else if (input == 4) {
-        fs.readFile('./data/pohutukawa.txt', 'utf8', (err, fileContents) => {
-            console.log(fileContents)
-            repeat()
-        })
-    }
-    else if (input == "q") {
-        process.exit()
-    }
-    else if (input == "c") {
-        const data = " and I am happy"
-        fs.appendFile('data/comments.txt', data, 'utf8', (err) => {
-            if (err) throw err;
-            console.log(data);
-        });
-    }
+    fs.readdir('data', 'utf8', (err, fileContents) => {
 
-    else {
-        console.log("No artwork for that number. Try again.")
-        repeat()
-    }
+        if (input <= fileContents.length) {
+            fs.readFile('./data/' + fileContents[input], 'utf8', (err, fileContents) => {
+                console.log(fileContents)
+            })
+        }
+        else if (input == "q") {
+            process.exit()
+        }
+        else if (input == "c") {
+            const data = " and I am happy"
+            fs.appendFile('data/comments.txt', data, 'utf8', (err) => {
+                if (err) throw err;
+                console.log(data);
+            });
+        }
+    })
 }
-
---
+++ tests/test.js
@@ -0,0 +1,18 @@
+const { expect } = require('@jest/globals')
+
+const func = require('../index.js')
+
+
+test('It works', () => {
+
+const file = "test.txt"
+
+const expected = "Hello"
+
+// readThisFile(file (data) => {
+//     const actual = data
+
+//     expect(actual).toEqual(expected)
+//     done()
+})
+
