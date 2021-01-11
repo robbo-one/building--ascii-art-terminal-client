5cbf825e4200d3d2ed54465f7b31e5300e7551e9
student <student@eda-bc-10.localdomain>
2021-01-11 15:13:44 +1300

    It works

---
+++ index.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+console.log("Hello art lovers");
+
+const readline = require("readline");
+
+function listOfPic() {
+  fs.readdir("./data", "utf8", (err, data) => {
+    pressEnter(data);
+    for (let i = 0; i < data.length; i++) {
+      return data[i];
+    }
+  })
+}
+
+listOfPic();
+
+function pressEnter(data) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  rl.question("Which file should I load? ", function (input) {
+    rl.close();
+    console.log(input, data);
+
+    // Call any functifs.reaons you like here. For example:
+
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i]);
+      if (input == i) {
+        fs.readFile("./data/" + data[i], "utf8", (err, data) => {
+          console.log(data);
+        })
+      }
+    }
+
+    // console.log(data)
+  })
+}
+
+// fs.readFile("listOfPic", "utf8", () => {
+//     console.log(listOfPic[input])
+// })
+
+// fs.readFile ('./data/kea.txt', "utf8", (err, data) => {
+//     console.log (data)
+// })
+
+// const prompt = require('prompt')
+
+// prompt.message = ''
+// prompt.delimiter = ': '
+// prompt.start()
+
+// const choice = {
+//   name: 'choice',
+//   hidden: true,
+//   message: 'Make your choice'
+// }
+
+// prompt.get(choice, function (err, result) {
+//  fs.readFile ("picture", "utf8", (err, data) => {
+//      console.log(picture[result.choice])
+// })
+// })
